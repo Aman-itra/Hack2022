e62c9890f68b1041ee88cf2d173797ecbb6ce64d
Aman-yt <116165694+Aman-yt@users.noreply.github.com>
2022-10-19 16:28:17 +0530

    Add files via upload

---
+++ Max_number_in_array.py
@@ -0,0 +1,31 @@
+# Python3 program to find maximum
+# in arr[] of size n
+
+# Function to find maximum
+# in arr[] of size n
+def largest(arr,n):
+# Initialize maximum element
+mx = arr[0]		
+
+# Traverse array elements from second		
+# and compare every element with		
+# current max		
+for i in range(1, n):		
+	if arr[i] > mx:		
+		mx = arr[i]
+		
+return mx
+
+# Driver Code
+arr = [10, 324, 45, 90, 9808]
+n = len(arr)
+
+#calculating length of an array
+Ans = largest(arr,n)
+
+# display max
+print ("Largest in given array is",Ans)
+
+# This code is contributed by Jai Parkash Bhardwaj
+# and improved by prophet1999
+
